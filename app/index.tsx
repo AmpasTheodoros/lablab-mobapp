8d6da824c0940946ff1a4f895fcb3a7a1f36469e
Theodor Ampas <71594408+AmpasTheodoros@users.noreply.github.com>
2025-01-04 00:55:00 +0200

    nativewind and some basic files

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { Link } from 'expo-router'
 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>App</Text>
+    <View className="flex-1 items-center justify-center bg-blue-600">
+      <Text className="text-3xl font-bold text-white">App</Text>
       <StatusBar style="auto" />
+      <Link href="/profile" className="mt-4 bg-white p-4 rounded-lg">
+        <Text>Profile</Text>
+      </Link>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
